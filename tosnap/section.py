ad26e032df69c08c11a0aac9a0c35decb967736e
Henri Drake <henrifdrake@gmail.com>
2024-10-26 20:41:19 -0400

    Placeholder for atlantic sections

--- tosnap/section.py
+++ tosnap/section.py
@@ -4,3 +4,6 @@ class Section():
     def __init__(self, coords):
         self.coords = coords
         self.lons, self.lats = np.array([lon for (lon, lat) in coords]), np.array([lat for (lon, lat) in coords])
+
+    def __repr__(self):
+        return self.coords
